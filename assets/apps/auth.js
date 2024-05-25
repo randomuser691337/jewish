9f6a088460cddd0dd6c3c7573038e6bb4f102500
randomuser691337 <randomuser691337@gmail.com>
2024-05-24 21:11:02 -0500

    okok

--- assets/apps/auth.js
+++ assets/apps/auth.js
@@ -11,16 +11,7 @@ async function authg() {
     const letmein = await ekey(i.value);
     if (letmein === true) {
         dest('oobespace'); eval(exeboot);
-    } else if (letmein === "missing") {
-        const f = document.getElementById('authp').value;
-        pass = f;
-        const fucker = await readf(`/system/check`);
-        if (fucker === "DontModifyOrYouWillBrickWebDesk") {
-            fesw('setupa', 'setupca');
-        } else {
-            snack('Wrong password!');
-        }
-     } else {
+    } else {
         snack('Wrong password!');
     }
 }
